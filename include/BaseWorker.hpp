c46a0bc82992e98f7f4c072a464f31ac1fb55adc
James Grantham <james.grantham@gmail.com>
2020-02-05 01:25:35 -0800

    added basica functionality and run loop thread

--- include/BaseWorker.hpp
+++ include/BaseWorker.hpp
@@ -1,11 +1,25 @@
 #ifndef BASEWORKER_HPP
 #define BASEWORKER_HPP
 
-class BaseWorker {
-  private:
-  bool on_{true};
+#include <chrono>
+#include <thread>
 
+class BaseWorker {
+  protected:
+  bool on_{true};
+  std::string workerName_;
+  std::thread workerThread_;
   public:
+  BaseWorker() {
+    workerName_ = __PRETTY_FUNCTION__;
+  }
+  virtual ~BaseWorker() {
+    std::cout << "Destroying: " << __PRETTY_FUNCTION__ << std::endl;
+  }
+  void start() {
+    std::cout << "Starting: " << workerName_ << std::endl;
+    workerThread_ = std::thread(&BaseWorker::run,this);
+  }
   virtual void prepare(){}
   virtual void cleanup(){}
   virtual void starting(){}
@@ -13,10 +27,16 @@ class BaseWorker {
   void run() {
     starting();
     while (on_) {
-      std::cout << "working: " << std::endl;
+      std::cout << "working: " << workerName_ << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(2));
     }
     finishing();
   }
+  void stop() {
+    on_ = false;
+  }
 };
+
+typedef void (BaseWorker::*WorkerMethod)();
 
 #endif
--- include/WorkerDispatcher.hpp
+++ include/WorkerDispatcher.hpp
@@ -1,15 +1,67 @@
 #include <BaseWorker.hpp>
+// #include <type_traits>
+// #include <cstddef>
 
+// template <typename T, typename... Ts>
+// constexpr std::size_t Index_v = Index<T, Ts...>::value;
+
+template <typename ...Workers>
 class WorkerDispatcher {
-  std::vector<BaseWorker> modules_;
+  std::vector<std::unique_ptr<BaseWorker>> workers_;
+  std::thread dispatcherThread_;
+  bool on_{true};
   public:
   WorkerDispatcher() {
-
+    std::cout << "Starting Dispatcher" << std::endl;
   }
 
-  template <typename ...Workers>
-  void initialize() {
-    std::cout << "initializing WorkerDispatcher" << std::endl;
-    (modules_.push_back(Workers()), ...);
+  void run() {
+    std::cout << "starting WorkerDispatcher" << std::endl;
+    (workers_.push_back(std::make_unique<Workers>()), ...);
+    std::cout << "created vector of workers" << std::endl;
+    ((std::cout << " name type: " << typeid(Workers).name() << std::endl), ...);
+    // ((std::cout << " name object: " << typeid(Workers).name() << std::endl), ...);
+    // (dynamic_cast<Workers>(workers_[i++]).run(), ...);
+    int i = 0;
+    (std::invoke(&Workers::prepare,dynamic_cast<Workers*>(workers_[i++].get())), ...);
+    i = 0;
+    (std::invoke(&Workers::start,dynamic_cast<Workers*>(workers_[i++].get())), ...);
+    while (on_) {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  }
+  void start() {
+    dispatcherThread_ = std::thread(&WorkerDispatcher::run,this);
+  }
+
+  void stop() {
+    int i = 0;
+    (std::invoke(&Workers::stop,dynamic_cast<Workers*>(workers_[i++].get())), ...);
+    on_ = false;
+    join();
+  }
+
+  void join() {
+    if (dispatcherThread_.joinable()) {
+      dispatcherThread_.join();
+    }
+  }
+
+  void prepareWorkers() {
+    for (auto &worker: workers_) {
+      worker->prepare();
+    }
+  }
+  void beginRunLoop() {
+    for (auto &worker: workers_) {
+      worker->run();
+    }
+  }
+
+  void runOnAllWorkers(WorkerMethod method) {
+    for (auto &worker : workers_) {
+
+      std::invoke(method, worker);
+    }
   }
 };
