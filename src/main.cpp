c91c1146dd348203015a9260f664146b4190032d
James Grantham <james.grantham@gmail.com>
2020-06-10 21:09:15 -0700

    add sample draw calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <GLFW/glfw3.h>
 #include <boost/program_options.hpp>
 #include <WorkerSystem.hpp>
 #include <Workers.hpp>
@@ -8,17 +9,57 @@ void signal_handler(int sig);
 
 
 Dispatcher dispatcher;
+void testGLFW();
 
 int main(int argc, char **argv) {
   std::cout << "Starting WorkerSystem" <<std::endl;
   signal(SIGINT, signal_handler); 
 
-  dispatcher.start();
-  dispatcher.join();
+  std::cout << "TESTTESTTEST" << std::endl;
+  testGLFW();
+  // dispatcher.start();
+  // dispatcher.join();
 }
 
 void signal_handler(int sig){ // can be called asynchronously
   std::cout << "shutting down" << std::endl;
   flag = 0; // set flag
   dispatcher.stop();
+}
+
+void testGLFW() {
+  GLFWwindow *window;
+  if (!glfwInit()) {
+    std::cout << "Could not init GLFW!" << std::endl;
+    exit(-1);
+  }
+
+
+  window = glfwCreateWindow(640,480, "Hello World", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    exit(-1);
+  }
+
+  glfwMakeContextCurrent(window);
+
+  char * version = (char*)glGetString(GL_VERSION);
+  std::cout << "GL VERSION: " << version << std::endl;
+
+  while(!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f,-0.5f);
+    glVertex2f(0.0f,0.5f);
+    glVertex2f(0.5f,-0.5f);
+    glEnd();
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+
+
+  }
+
+  glfwTerminate();
 }
