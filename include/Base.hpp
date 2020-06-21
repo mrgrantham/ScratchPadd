c8747513b09201d3081d831a8506593ae1ba8d34
James Grantham <james.grantham@gmail.com>
2020-06-21 16:22:35 -0700

    added support for work queue and interval work repetition

--- include/Base.hpp
+++ include/Base.hpp
@@ -5,7 +5,7 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 #include <EventTimer.hpp>
-
+#include <boost/lockfree/queue.hpp>
 
 inline std::string className(const std::string& classMethod)
 {
@@ -26,46 +26,90 @@ namespace ScratchPadd {
 class Base {
   protected:
   bool on_{true};
-  std::string workerName_;
+  std::string paddName_;
   std::thread workerThread_;
   int repeating_interval_{0};
+  int work_thread_sleep_interval_{1000};
   EventTimer repeatingTimer_;
+  boost::lockfree::queue<std::function<void()>*> work_queue_{100};
+
   public:
   Base() {
-    workerName_ = __CLASS_NAME__;
+    paddName_ = __CLASS_NAME__;
   }
   virtual ~Base() {
     // spdlog gets torn down before the destructor
     // need cout in order to know about destructors called
     // spdlog::info("Destroying: {}", __CLASS_NAME__);
-    std::cout <<  "Destroying: " << __CLASS_NAME__ << std::endl;
+    repeatingTimer_.stop();
+    std::cout <<  "Base() Destroying: " << paddName_ << std::endl;
   }
   void start() {
-    spdlog::info("Starting: {}", workerName_ );
+    spdlog::info("Starting: {}", paddName_ );
     workerThread_ = std::thread(&Base::run,this);
+    startRepeater();
   }
   virtual void config(){}
   virtual void prepare(){}
   virtual void cleanup(){}
   virtual void starting(){}
   virtual void finishing(){}
-  virtual void repeat(){}
+  virtual void repeat(){
+    spdlog::info("Base::repeat() called. Padd set a repeat interval of {} with no method override",repeating_interval_);
+  }
 
   void run() {
     starting();
     while (on_) {
-      spdlog::info("Working: {}", workerName_);
-      std::this_thread::sleep_for(std::chrono::seconds(2));
+      std::function<void()> *work = nullptr;
+      while(work_queue_.pop(work)) {
+        if (!work) spdlog::error("work is null");
+        work->operator()();
+        delete work;
+      }
+      // spdlog::warn("sleeping work_queue_ {} for {}ms",paddName_, work_thread_sleep_interval_);
+      std::this_thread::sleep_for(std::chrono::milliseconds(work_thread_sleep_interval_));
     }
     finishing();
   }
 
-  void setRepeater(int repeat_interval) {
+  void setRepeatInterval( int interval) {
+    repeating_interval_ = interval;
+  }
 
+  void startRepeater() {
+    if (repeating_interval_) {
+      spdlog::info("repeat() interval set to {}",repeating_interval_);
+      repeatingTimer_.startRepeatingEvent([=]{
+        std::function<void()> work = std::function<void()>([=]{
+          this->repeat();
+        });
+        send(work);
+      },repeating_interval_);
+    } else {
+      spdlog::info("No repeat() interval set");
+    }
+  }
+
+  template <typename Sendable>
+  void send(Sendable sendable) {
+    std::function<void()> *function = new std::function<void()>([=]{
+      recieve(sendable);
+    });
+    work_queue_.push(function);
+  }
+
+  void recieve(const std::function<void()> &work) {
+    work();
+  }
+
+
+  void recieve(void) {
+    spdlog::error("Void Receive Called");
   }
 
   void stop() {
-    spdlog::info("Stopping: {}", workerName_ );
+    spdlog::info("Stopping: {}", paddName_ );
     on_ = false;
     if (workerThread_.joinable()) {
       workerThread_.join();
