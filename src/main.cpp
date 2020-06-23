72a62d6ca670fdf12b941fc58e9a2d165640f198
James Grantham <james.grantham@gmail.com>
2020-06-23 08:21:42 -0700

    no longer instantiates Scratchpadd system class

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,20 @@ volatile sig_atomic_t flag = 0;
 void signal_handler(int sig);
 
 
-ScratchPadd::System<SCRATCHPADD_GL4> SPSystem;
+typedef ScratchPadd::System<SCRATCHPADD_GL4> SPSystem;
 
 int main(int argc, char **argv) {
   SCOPED_METHOD_TIMER();
   signal(SIGINT, signal_handler); 
   spdlog::info("Welcome to SCRATCHPADD!");
-  SPSystem.instantiate();
-  SPSystem.start();
+  SPSystem::instantiate();
+  SPSystem::start();
   std::this_thread::sleep_for(std::chrono::seconds(5));
-  SPSystem.stop();
+  SPSystem::stop();
 }
 
 void signal_handler(int sig){ // can be called asynchronously
   spdlog::info("shutting down");
   flag = 0; // set flag
-  SPSystem.stop();
+  SPSystem::stop();
 }
