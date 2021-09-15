cb583b39f4cca0a001c996d234a2f85c4de0280f
James Grantham <james.grantham@gmail.com>
2021-09-15 00:02:09 -0700

    add imgui opengl backend

--- include/GL.hpp
+++ include/GL.hpp
@@ -1,6 +1,9 @@
 #include <GL/glew.h>  // include before GLFW
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 float positions[] = {
      -0.5f, -0.5f, 
@@ -29,6 +32,11 @@ class GL2 : public Graphics {
   void tearDown() override {}
 };
 
+static void glfw_error_callback(int error, const char* description)
+{
+    spdlog::error("Glfw Error {}: {}\n", error, description);
+}
+
 class GL4 : public Graphics {
   unsigned int shaders = 0;
   std::string vertexShader =
@@ -51,9 +59,17 @@ class GL4 : public Graphics {
       "   color =  vec4(1.0,0.5,0.5,0.5);\n"
       "}\n";
 
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // const char* glsl_version = "#version 330 core";
+    const char* glsl_version = "#version 330 core";
+
  public:
   GLFWwindow *window;
   void setupWindow() override {
+    glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
       spdlog::error("Could not init GLFW!");
       exit(-1);
@@ -69,7 +85,19 @@ class GL4 : public Graphics {
       glfwTerminate();
       exit(-1);
     }
+    spdlog::info("Window Created!");
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
     glewExperimental = GL_TRUE;
     GLenum status = glewInit();
@@ -104,18 +132,76 @@ class GL4 : public Graphics {
 
   bool draw() override {
     if (!glfwWindowShouldClose(window)) {
+      spdlog::info("window on display");
       checkOpenGLErrors();
+      glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+      // Rendering
+      ImGui::Render();
+      int display_w, display_h;
+      glfwGetFramebufferSize(window, &display_w, &display_h);
+      glViewport(0, 0, display_w, display_h);
+      glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
       glClear(GL_COLOR_BUFFER_BIT);
       glDrawArrays(GL_TRIANGLES, 0, 3);
+      
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
       glfwSwapBuffers(window);
-      glfwPollEvents();
       return true;
     }
     return false;
   }
 
   void tearDown() override {
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     glDeleteProgram(shaders);
+    glfwDestroyWindow(window);
     glfwTerminate();
   }
 };
