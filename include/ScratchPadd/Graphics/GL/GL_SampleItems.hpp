ea1a1111755bbb5b8287185046f478ad2d69053f
James Grantham <james.grantham@gmail.com>
2021-09-23 01:41:53 -0700

    make sample shape a parallelagram

--- include/ScratchPadd/Graphics/GL/GL_SampleItems.hpp
+++ include/ScratchPadd/Graphics/GL/GL_SampleItems.hpp
@@ -10,11 +10,12 @@ float positions[] = {
 std::vector<Graphics::VertexPair> vertices = {
       {{-0.5f, -0.5f, 0.0f},{0.0f, 0.0f, 0.0f}},
       {{0.0f, 0.5f, 0.0f},{0.0f, 0.0f, 0.0f}},
-      {{0.5f, -0.5f, 0.0f},{0.0f, 0.0f, 0.0f}}
+      {{0.5f, -0.5f, 0.0f},{0.0f, 0.0f, 0.0f}},
+      {{1.0f, 0.5f, 0.0f},{0.0f, 0.0f, 0.0f}}
 };
 
 std::vector<uint32_t> indices = {
-  0, 1, 2
+  0, 1, 2, 1, 2, 3
 };
 
 namespace ScratchPadd {
